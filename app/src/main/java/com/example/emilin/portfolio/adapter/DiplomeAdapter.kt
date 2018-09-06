12480919d31c9fcaff0e8dfd5c9df4d027dc94dd
Emilin <dadie.emilin@gmail.com>
2018-09-06 23:34:27 +0200

    diplomeFragement bgcolor

--- app/src/main/java/com/example/emilin/portfolio/adapter/DiplomeAdapter.kt
+++ app/src/main/java/com/example/emilin/portfolio/adapter/DiplomeAdapter.kt
@@ -1,6 +1,10 @@
+
 package com.example.emilin.portfolio.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -12,6 +16,10 @@ import com.example.emilin.portfolio.R
 import com.example.emilin.portfolio.extension.loadUrl
 import com.example.emilin.portfolio.model.Diplome
 import java.util.ArrayList
+import android.content.res.TypedArray
+import android.graphics.Color
+import android.widget.Toast
+import com.example.emilin.portfolio.R.array.colorDiplome
 
 
 class DiplomeAdapter(val context: Context, val onItemClick: OnItemClick): RecyclerView.Adapter<DiplomeAdapter.ViewHolder>() {
@@ -31,19 +39,23 @@ class DiplomeAdapter(val context: Context, val onItemClick: OnItemClick): Recycl
         return diplomesList.size
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
+    @SuppressLint("ResourceType", "Recycle")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         mClickListener = onItemClick
         val currentDiplome = diplomesList[position]
         holder?.diplome_title?.text = currentDiplome.name
         holder?.diplome_image?.loadUrl(currentDiplome.picture_url)
 
+        val arrayOfStrings = context.getResources().getStringArray(R.array.colorDiplome) // one, two
+        holder?.diplome_card?.setBackgroundColor(Color.parseColor(arrayOfStrings[position]))
         holder?.itemView?.setOnClickListener{
             onItemClick.click(diplomesList[position], holder.adapterPosition)
         }
     }
 
     class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
-        val diplomeCard = itemView?.findViewById<CardView>(R.id.diplome_card)
+        val diplome_card = itemView?.findViewById<CardView>(R.id.diplome_card)
         val diplome_image = itemView?.findViewById<ImageView>(R.id.diplome_item_image)
         val diplome_title = itemView?.findViewById<TextView>(R.id.diplome_item_title)
     }
